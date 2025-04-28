378c2a4a5b1050665d97bb799b6765acf2ac676c
Victor Daniel Venegas Gonzalez <victorvenegas07@gmail.com>
2025-04-27 20:53:22 -0500

    Actualiza enlaces de registro y recuperación de contraseña en el formulario de inicio de sesión; modifica la URL de la API en los entornos de desarrollo.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: false,
-    API_URL: 'http://localhost:5276/api',
+    API_URL: 'https://resenaslibros-gacfaje8g2fahycf.eastus2-01.azurewebsites.net/api',
 };
